44850009df8db83a28b1b53b892d35cd93fc6656
jyoh <ojy1018@gmail.com>
2022-12-01 15:01:54 +0900

    login with session,cookie

--- src/server.js
+++ src/server.js
@@ -1,19 +1,30 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import {localsMiddleware} from "./middlewares";
 
 const app = express(); // express app 생성
+const logger = morgan("dev"); // logger middleware: dev, combined, common, short, tiny
 
-const logger = morgan("dev"); // dev, combined, common, short, tiny
-
+// app settings
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
+// app middleware
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+}));
 
+app.use(localsMiddleware);
+
+// app router
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
